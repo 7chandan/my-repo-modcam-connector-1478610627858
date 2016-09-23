16c85a47444fa3f94aff584255f923a6b6b830dd
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-09-23 12:33:02 +0200

    Add "year" projection

--- app/http/routes/peoplecount.js
+++ app/http/routes/peoplecount.js
@@ -2,7 +2,7 @@ const express = require('express')
 const log = require('winston')
 const util = require('../../util')
 const peoplecount = require('../../database/peoplecount')
-const projections = ['day', 'hour', 'minute']
+const projections = ['year', 'day', 'hour', 'minute']
 
 const router = express.Router()
 
